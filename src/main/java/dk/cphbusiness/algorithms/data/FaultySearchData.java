8bb357dd0db1e15e117bf66d59254bb108c6c0fb
Anders Kalhauge <anders@kalhauge.dk>
2019-05-15 08:47:54 +0200

    javadoc added

--- src/main/java/dk/cphbusiness/algorithms/data/FaultySearchData.java
+++ src/main/java/dk/cphbusiness/algorithms/data/FaultySearchData.java
@@ -9,6 +9,15 @@ import java.util.Set;
 public class FaultySearchData {
   private static final Random random = new Random(7);
   
+  /**
+   * Create sorted data with selected faulty data points.
+   * Writes double values in ascending order to a stream.
+   * 
+   * @param out the stream to write to
+   * @param size number of data points
+   * @param max maximum value
+   * @param faultArray array of indexes for faulty points
+   */
   public static void createData(
       PrintStream out,
       long size, 
@@ -18,8 +27,7 @@ public class FaultySearchData {
     Set<Long> faults = new HashSet<>();
     for (long fault : faultArray) faults.add(fault);
     double number = 0.0;
-    out.println(number);
-    for (long index = 1; index < size; index++) {
+    for (long index = 0; index < size; index++) {
       double delta = random.nextDouble()*2*(max - number)/(size - index);
       number += delta;
       if (faults.contains(index)) out.println(-random.nextDouble()*2*number);
@@ -32,6 +40,5 @@ public class FaultySearchData {
     PrintStream file = new PrintStream("/Users/AKA/tmp/faulty-1024.txt");
     createData(file, 1024, 1_000_000.0, 256, 512, 734);
     }
-  
 
   }
